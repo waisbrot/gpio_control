b30342ce85009bf1c80348e8dbf7997ba52b1a8d
Nathaniel Waisbrot <code@waisbrot.net>
2021-03-30 23:27:32 -0400

    influx fixes

--- gpio_control/temperature.py
+++ gpio_control/temperature.py
@@ -4,15 +4,18 @@ import os
 import board
 import adafruit_si7021
 from socket import gethostname
-from influxdb_client.client import write_api as write_influx
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS
 
 class Temperature(Thread):
     def __init__(self) -> None:
         super().__init__(group=None, name='temperature measure', daemon=False)
         self._bucket = os.getenv('INFLUX_BUCKET')
-        self._org = os.getenv('INFLUX_ORG')
-        self._token = os.getenv('INFLUX_TOKEN')
+        org = os.getenv('INFLUX_ORG')
+        token = os.getenv('INFLUX_TOKEN')
         self._hostname = gethostname()
+        client = InfluxDBClient(url="http://tularemia.home.waisbrot.net:8086", token=token, org=org)
+        self.write_influx = client.write_api(write_options=SYNCHRONOUS)
 
     def run(self) -> None:
         sensor = adafruit_si7021.SI7021(board.I2C())
@@ -21,5 +24,5 @@ class Temperature(Thread):
                 f'temperature,host={self._hostname} degrees_c={sensor.temperature}',
                 f'humidity,host={self._hostname} humidity_pct={sensor.relative_humidity}',
             ]
-            write_influx.write(self._bucket, self._org, metrics)
+            write_influx.write(bucket=self._bucket, record=metrics)
             sleep(10)
