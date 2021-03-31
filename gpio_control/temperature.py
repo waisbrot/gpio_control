265b5ed7d3318f8f84e423437382112c33b583bf
Nathaniel Waisbrot <code@waisbrot.net>
2021-03-31 19:51:57 -0400

    fix influx stuff

--- gpio_control/temperature.py
+++ gpio_control/temperature.py
@@ -1,3 +1,4 @@
+import logging
 from threading import Thread
 from time import sleep
 import os
@@ -7,14 +8,17 @@ from socket import gethostname
 from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
 
+log = logging.getLogger('main')
+
 class Temperature(Thread):
     def __init__(self) -> None:
         super().__init__(group=None, name='temperature measure', daemon=False)
         self._bucket = os.getenv('INFLUX_BUCKET')
         org = os.getenv('INFLUX_ORG')
         token = os.getenv('INFLUX_TOKEN')
+        url = os.getenv("INFLUX_URL")
         self._hostname = gethostname()
-        client = InfluxDBClient(url="http://tularemia.home.waisbrot.net:8086", token=token, org=org)
+        client = InfluxDBClient(url=url, token=token, org=org)
         self.write_influx = client.write_api(write_options=SYNCHRONOUS)
 
     def run(self) -> None:
@@ -24,5 +28,6 @@ class Temperature(Thread):
                 f'temperature,host={self._hostname} degrees_c={sensor.temperature}',
                 f'humidity,host={self._hostname} humidity_pct={sensor.relative_humidity}',
             ]
-            write_influx.write(bucket=self._bucket, record=metrics)
+            self.write_influx.write(bucket=self._bucket, record=metrics)
+            log.debug(f"Wrote to influx: {metrics}")
             sleep(10)
