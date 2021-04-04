4521411dda8937675d63f9a8b4794d2d7e5e0229
Nathaniel Waisbrot <code@waisbrot.net>
2021-04-04 12:27:13 -0400

    Improve logging on sensor

--- gpio_control/temperature.py
+++ gpio_control/temperature.py
@@ -18,16 +18,18 @@ class Temperature(Thread):
         token = os.getenv('INFLUX_TOKEN')
         url = os.getenv("INFLUX_URL")
         self._hostname = gethostname()
+        log.debug("Initializing Influx client")
         client = InfluxDBClient(url=url, token=token, org=org)
         self.write_influx = client.write_api(write_options=SYNCHRONOUS)
 
     def run(self) -> None:
+        log.debug("Initializing temp sensor")
         sensor = adafruit_si7021.SI7021(board.I2C())
         while True:
             metrics = [
                 f'temperature,host={self._hostname} degrees_c={sensor.temperature}',
                 f'humidity,host={self._hostname} humidity_pct={sensor.relative_humidity}',
             ]
+            log.debug(f"Going to write to influx: {metrics}")
             self.write_influx.write(bucket=self._bucket, record=metrics)
-            log.debug(f"Wrote to influx: {metrics}")
             sleep(10)
